e17debfaf069ad18fecdf28b9f82ed6cd515260b
tamami <tamami@tamami-pc>
2014-09-15 08:20:34 +0700

    penambahan fungsi untuk mengambil store procedure sesuaikan_pembayaran

--- src/lab/aikibo/manager/SPManager.java
+++ src/lab/aikibo/manager/SPManager.java
@@ -1,8 +1,11 @@
 package lab.aikibo.manager;
 
+import java.math.BigDecimal;
 import java.sql.CallableStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.hibernate.Session;
 
@@ -42,6 +45,18 @@ public class SPManager {
 				prepareCall("{call update_sppt_ke_batal(?,?)}");
 		cs.setString(1, nop);
 		cs.setString(2, thn);
+		cs.executeQuery();
+	}
+	
+	public void callSesuaikanPembayaran(String nop, String thn, BigDecimal nilai, BigDecimal denda, Calendar tglTransaksi, Calendar tglCatat) throws SQLException {
+		CallableStatement cs = boneCP.getBoneCPConn().prepareCall("{call sesuaikan_pembayaran(?,?,?,?,?,?)}");
+		cs.setString(1, nop);
+		cs.setString(2, thn);
+		cs.setBigDecimal(3, nilai);
+		//java.sql.Date d = 
+		cs.setDate(4, (java.sql.Date) tglTransaksi.getTime());
+		cs.setDate(5, (java.sql.Date) tglCatat.getTime());
+		cs.setBigDecimal(6, denda);
 		cs.executeQuery();
 	}
 
