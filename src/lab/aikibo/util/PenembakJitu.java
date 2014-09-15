e443d0f1f9c76d037b508a108de868c13cba5213
tamami <tamami@tamami-pc>
2014-09-15 08:21:11 +0700

    tambah flag 4 untuk sesuaikan pembayaran

--- src/lab/aikibo/util/PenembakJitu.java
+++ src/lab/aikibo/util/PenembakJitu.java
@@ -2,7 +2,10 @@ package lab.aikibo.util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -56,6 +59,30 @@ public class PenembakJitu {
 					callSP.callUpdateSpptKeBatal(nop, thn);
 				} catch(SQLException sqle) {
 					System.err.println("Error SQL Exception saat mengubah status SPPT jadi batal");
+					sqle.printStackTrace();
+					return false;
+				}
+			}
+			// panggil sp sesuaikan_pembayaran
+			if(flag.equals("4")) {
+				String nilaiTx = scnData.next();
+				String dendaTx = scnData.next();
+				String tglTransaksiTx = scnData.next();
+				String tglCatatTx = scnData.next();
+				BigDecimal nilai = new BigDecimal(nilaiTx);
+				BigDecimal denda = new BigDecimal(dendaTx);
+				Calendar tglTransaksi = Calendar.getInstance();
+				  tglTransaksi.set(Calendar.DATE, Integer.parseInt(tglTransaksiTx.substring(0,2)));
+				  tglTransaksi.set(Calendar.MONTH, Integer.parseInt(tglTransaksiTx.substring(2, 4))-1);
+				  tglTransaksi.set(Calendar.YEAR, Integer.parseInt(tglTransaksiTx.substring(4,8)));
+				Calendar tglCatat = Calendar.getInstance();
+				  tglCatat.set(Calendar.DATE, Integer.parseInt(tglCatatTx.substring(0,2)));
+				  tglCatat.set(Calendar.MONTH, Integer.parseInt(tglCatatTx.substring(2,4))-1);
+				  tglCatat.set(Calendar.YEAR, Integer.parseInt(tglCatatTx.substring(4,8)));
+				try {
+					callSP.callSesuaikanPembayaran(nop, thn, nilai, denda, tglTransaksi, tglCatat);
+				} catch(SQLException sqle) {
+					System.err.println("Error SQL Exception saat menyesuaikan pembayaran SPPT");
 					sqle.printStackTrace();
 					return false;
 				}
